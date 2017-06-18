eff0bfe38520a815a8168ab906c04cb9731abba0
lioran1990 <lior.ran1990@gmail.com>
2017-06-18 22:26:30 +0300

    removed dist/out files from git

--- angular-src/dist/out-tsc/app/components/testcompo/testcompo.component.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=C:/Users/Lioran/WebstormProjects/SupermarketProject/angular-src/src/app/components/testcompo/testcompo.component.js.map
--- angular-src/dist/out-tsc/environments/environment.prod.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: true
-};
-//# sourceMappingURL=C:/Users/Lioran/WebstormProjects/SupermarketProject/angular-src/src/environments/environment.prod.js.map
