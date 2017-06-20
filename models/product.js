1c037dc49fb9afe91abf86e17aace88c7566ba70
ArtiomPetrov <artiom.petrov@whitesourcesoftware.com>
2017-06-20 20:36:04 +0300

    Add electronic products

--- models/product.js
+++ models/product.js
@@ -75,152 +75,152 @@ var productCreator = function () {
 //Dairy  products
 var productNum1 =  new productCreator();
 productNum1.serialNumber = 1;
-productNum1.productName = 'milk';
-productNum1.productCategory = 'Dairy';
+productNum1.productName = 'Sony Playstation 4';
+productNum1.productCategory = 'Game Consoles';
 productNum1.weightable = false;
-productNum1.productPrice = 8;
-productNum1.productManufacturer = 'tnova';
+productNum1.productPrice = 1000;
+productNum1.productManufacturer = 'Sony';
 productNum1.productStoreID = 10;
 
 
 var productNum2 =  new productCreator();
 productNum2.serialNumber = 2;
-productNum2.productName = 'Yogurt';
-productNum2.productCategory = 'Dairy ';
+productNum2.productName = 'Air Conditioner';
+productNum2.productCategory = 'Electronics';
 productNum2.weightable = false;
-productNum2.productPrice = 9;
-productNum2.productManufacturer = 'tnova';
+productNum2.productPrice = 800;
+productNum2.productManufacturer = 'Tadiran';
 productNum2.productStoreID = 10;
 
 
 var productNum3 =  new productCreator();
 productNum3.serialNumber = 3;
-productNum3.productName = 'Cheese';
-productNum3.productCategory = 'Dairy';
+productNum3.productName = 'Television';
+productNum3.productCategory = 'Electronics';
 productNum3.weightable = false;
 productNum3.productPrice = 7;
-productNum3.productManufacturer = 'tnova';
+productNum3.productManufacturer = 'Philips';
 productNum3.productStoreID = 10;
 
 
 var productNum4 =  new productCreator();
 productNum4.serialNumber = 4;
-productNum4.productName = 'Chocolate';
-productNum4.productCategory = 'Dairy';
+productNum4.productName = 'Lamp';
+productNum4.productCategory = 'Lights';
 productNum4.weightable = false;
 productNum4.productPrice = 4;
-productNum4.productManufacturer = 'tnova';
+productNum4.productManufacturer = 'Electra';
 productNum4.productStoreID = 10;
 
 
 var productNum5 =  new productCreator();
 productNum5.serialNumber = 5;
-productNum5.productName = 'Cream';
-productNum5.productCategory = 'Dairy';
+productNum5.productName = 'Electric Bike';
+productNum5.productCategory = 'Electronics';
 productNum5.weightable = false;
 productNum5.productPrice = 5;
-productNum5.productManufacturer = 'tnova';
+productNum5.productManufacturer = 'Koning';
 productNum5.productStoreID = 10;
 
 
 //Meat products
 var productNum6 =  new productCreator();
 productNum6.serialNumber = 6;
-productNum6.productName = 'prime rib';
-productNum6.productCategory = 'Meat';
+productNum6.productName = 'Microwave';
+productNum6.productCategory = 'Electronics';
 productNum6.weightable = true;
 productNum6.productPrice = 8;
-productNum6.productManufacturer = 'Red red';
+productNum6.productManufacturer = 'Galantz';
 productNum6.productStoreID = 10;
 
 
 var productNum7 =  new productCreator();
 productNum7.serialNumber = 7;
-productNum7.productName = 'Sirloin';
-productNum7.productCategory = 'Meat';
+productNum7.productName = 'Laptop';
+productNum7.productCategory = 'Gadgets';
 productNum7.weightable = true;
 productNum7.productPrice = 7;
-productNum7.productManufacturer = 'Red red';
+productNum7.productManufacturer = 'Lenovo';
 productNum7.productStoreID = 10;
 
 
 var productNum8 =  new productCreator();
 productNum8.serialNumber = 8;
-productNum8.productName = 'Kebab';
-productNum8.productCategory = 'Meat';
+productNum8.productName = 'MacBook';
+productNum8.productCategory = 'Gadgets';
 productNum8.weightable = true;
 productNum8.productPrice = 9;
-productNum8.productManufacturer = 'Red red';
+productNum8.productManufacturer = 'Apple';
 productNum8.productStoreID = 10;
 
 
 var productNum9 =  new productCreator();
 productNum9.serialNumber = 9;
-productNum9.productName = 'Hot dogs';
-productNum9.productCategory = 'Meat';
+productNum9.productName = 'iPhone';
+productNum9.productCategory = 'Communications';
 productNum9.weightable = true;
 productNum9.productPrice = 6;
-productNum9.productManufacturer = 'Red red';
+productNum9.productManufacturer = 'Apple';
 productNum9.productStoreID = 10;
 
 
 var productNum10 =  new productCreator();
 productNum10.serialNumber = 10;
-productNum10.productName = 'Flaps';
-productNum10.productCategory = 'Meat';
+productNum10.productName = 'LG g5';
+productNum10.productCategory = 'Communications';
 productNum10.weightable = true;
 productNum10.productPrice = 9;
-productNum10.productManufacturer = 'Red red';
+productNum10.productManufacturer = 'LG';
 productNum10.productStoreID = 10;
 
 //Vegetables
 var productNum11 =  new productCreator();
 productNum11.serialNumber = 11;
-productNum11.productName = 'Cucumber';
-productNum11.productCategory = 'Vegetables';
+productNum11.productName = 'Xbox';
+productNum11.productCategory = 'Game consoles';
 productNum11.weightable = true;
 productNum11.productPrice = 3;
-productNum11.productManufacturer = 'fruit cylinder';
+productNum11.productManufacturer = 'Microsoft';
 productNum11.productStoreID = 10;
 
 
 var productNum12 =  new productCreator();
 productNum12.serialNumber = 12;
-productNum12.productName = 'Tomato';
-productNum12.productCategory = 'Vegetables';
+productNum12.productName = 'Refrigerator';
+productNum12.productCategory = 'Electronics';
 productNum12.weightable = true;
 productNum12.productPrice = 2;
-productNum12.productManufacturer = 'fruit cylinder';
+productNum12.productManufacturer = 'Electra';
 productNum12.productStoreID = 10;
 
 
 var productNum13 =  new productCreator();
 productNum13.serialNumber = 13;
-productNum13.productName = 'Onion';
-productNum13.productCategory = 'Vegetables';
+productNum13.productName = 'Iron';
+productNum13.productCategory = 'Clothing';
 productNum13.weightable = true;
 productNum13.productPrice = 1;
-productNum13.productManufacturer = 'fruit cylinder';
+productNum13.productManufacturer = 'Philips';
 productNum13.productStoreID = 10;
 
 
 var productNum14 =  new productCreator();
 productNum14.serialNumber = 14;
-productNum14.productName = 'Gamba';
-productNum14.productCategory = 'Vegetables';
+productNum14.productName = 'Shaving machine';
+productNum14.productCategory = 'Hygiene';
 productNum14.weightable = true;
 productNum14.productPrice = 6;
-productNum14.productManufacturer = 'fruit cylinder';
+productNum14.productManufacturer = 'Philips';
 productNum14.productStoreID = 10;
 
 
 var productNum15 =  new productCreator();
 productNum15.serialNumber = 15;
-productNum15.productName = 'Sweet potato';
-productNum15.productCategory = 'Vegetables';
+productNum15.productName = 'Toaster';
+productNum15.productCategory = 'Electronics';
 productNum15.weightable = true;
 productNum15.productPrice = 4;
-productNum15.productManufacturer = 'fruit cylinder';
+productNum15.productManufacturer = 'Kennedy';
 productNum15.productStoreID = 10;
 
 
