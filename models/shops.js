de1808852515306e7e113abc669e8a40e09effc5
ArtiomPetrov <artiom.petrov@whitesourcesoftware.com>
2017-06-20 20:49:27 +0300

    Refactor shops

--- models/shops.js
+++ models/shops.js
@@ -57,58 +57,58 @@ var shopCreator = function () {
 
 var shopNum1 =  new shopCreator();
 shopNum1.storeId = 1;
-shopNum1.shopBranchName = 'chen';
-shopNum1.shopCompany = 'Tiv Taam';
+shopNum1.shopBranchName = 'Mahsaney Hashmal';
+shopNum1.shopCompany = 'Mahsaney Hashmal';
 shopNum1.shopSize = 5;
-shopNum1.lat = 31.9696535;
-shopNum1.lng = 34.77270108;
+shopNum1.lat = 32.075869;
+shopNum1.lng = 34.795875;
 shopNum1.rating = '10';
-shopNum1.city = 'Rishon Lezion';
+shopNum1.city = 'Tel Aviv';
 
 
 
 var shopNum2 =  new shopCreator();
 shopNum2.storeId = 1;
-shopNum2.shopBranchName = 'dvir';
-shopNum2.shopCompany = 'Shufersal';
+shopNum2.shopBranchName = 'Traklin Hashmal';
+shopNum2.shopCompany = 'Traklin Hashmal';
 shopNum2.shopSize = 4;
-shopNum2.lat = 31.97595157;
-shopNum2.lng = 34.79046535;
+shopNum2.lat = 32.074433;
+shopNum2.lng = 34.795597;
 shopNum2.rating = '5';
-shopNum2.city = 'Rishon Lezion';
+shopNum2.city = 'Tel Aviv';
 
 
 
 var shopNum3 =  new shopCreator();
 shopNum3.storeId = 1;
-shopNum3.shopBranchName = 'shahar';
-shopNum3.shopCompany = 'Yeinot Bitan';
+shopNum3.shopBranchName = 'Arena Mall';
+shopNum3.shopCompany = 'Arena Mall';
 shopNum3.shopSize = 3;
-shopNum3.lat = 31.98629696;
-shopNum3.lng = 34.76187515;
+shopNum3.lat = 32.192284;
+shopNum3.lng = 34.853500;
 shopNum3.rating = '2';
-shopNum3.city = 'Rishon Lezion';
+shopNum3.city = 'Herzliya';
 
 var shopNum4 =  new shopCreator();
 shopNum4.storeId = 3;
-shopNum4.shopBranchName = 'Shay';
-shopNum4.shopCompany = 'Mega';
+shopNum4.shopBranchName = 'Ofer Grand Mall';
+shopNum4.shopCompany = 'Ofer Grand Mall';
 shopNum4.shopSize = 2;
-shopNum4.lat = 32.01104542;
-shopNum4.lng = 34.79431915;
+shopNum4.lat = 31.250243;
+shopNum4.lng = 34.771875;
 shopNum4.rating = '1';
-shopNum4.city = 'Holon';
+shopNum4.city = 'Beer Sheva';
 
 
 var shopNum5 =  new shopCreator();
 shopNum5.storeId = 2;
-shopNum5.shopBranchName = 'Ron';
-shopNum5.shopCompany = 'Rami Levi';
+shopNum5.shopBranchName = 'Dizengoff Mall';
+shopNum5.shopCompany = 'Dizengoff Mall';
 shopNum5.shopSize = 4;
-shopNum5.lat = 31.9988176;
-shopNum5.lng = 34.7514038;
+shopNum5.lat = 32.075346;
+shopNum5.lng = 34.775395;
 shopNum5.rating = '5';
-shopNum5.city = 'Bat Yam';
+shopNum5.city = 'Tel Aviv';
 
 var shop1 = new Shop(shopNum1);
 var shop2 = new Shop(shopNum2);
